71a7064b57eabd5158618dc04f8dc21bb467b8ed
Pedro Blumenschein <pedroblumenschein@gmail.com>
2020-11-21 00:00:56 +0100

    day1 commit2 - fix of slack webhook URL

--- routes/schedule.js
+++ routes/schedule.js
@@ -2,13 +2,17 @@ const express = require('express');
 const router  = express.Router();
 const Axios = require('axios');
 
+const sendMessageSlackWebhook = 'https://hooks.slack.com/services/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';
+
 //functions
 const okMessage = (obj) => {
     console.log(obj)
     Axios
-      .post('https://hooks.slack.com/services/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', 
+      .post( sendMessageSlackWebhook, 
       {text: `Hey ${obj.user_name}, your session has been scheduled! This message was sent from the real project 🧠`,
     })
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
 }
 
 //routes 
@@ -21,10 +25,10 @@ router.post('/', (req, res, next) => {
     okMessage(req.body)
   });
 
-// This was added just so we can trigger a slack message from the web. it is optional.
+// This is a form added just so we can trigger a slack message from the web. it is optional.
 router.post('/sendText', (req, res, next) => {
     Axios
-    .post('https://hooks.slack.com/services/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', 
+    .post(sendMessageSlackWebhook, 
     {text: `${req.body.content}`,
   })
   .then( data => {
