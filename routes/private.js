5a6e5a08e815240d0f1317221b2f877e9b2a7124
Facundo Gomez <gomezvera.f@gmail.com>
2020-11-24 12:38:41 +0100

    add close session action in overview

--- routes/private.js
+++ routes/private.js
@@ -29,18 +29,17 @@ router.get('/dashboard', middlewares.loginCheck, (req, res, next) => {
     HelpSession.find().populate('student')
     .populate('teacher')
     .then(sessions => {
-      // render a books view to display them
-      
-      // sessions.sessionStartDate = 14/07/2020
-      sessions.forEach( (sess)=>{
-        console.log(sess);
-        sess.description = sess.sessionStartDate.toString().split(' ')[4]
-      // sess[hours] = sess.sessionStartDate.toString().split(' ')[4]
-      // sess.sessionStartDate = sess.sessionStartDate
-      })
-      // sessions.sessionStartDate.getHours() + ":" + sessions.sessionStartDate.getMinutes() + ":" + sessions.sessionStartDate.getSeconds();
-      // console.log(sessions)
-      // console.log(sessions)
+      // render a books view to display them      
+      // // sessions.sessionStartDate = 14/07/2020
+      // sessions.forEach( (sess)=>{
+      //   console.log(sess);
+      //   sess.description = sess.sessionStartDate.toString().split(' ')[4]
+      // // sess[hours] = sess.sessionStartDate.toString().split(' ')[4]
+      // // sess.sessionStartDate = sess.sessionStartDate
+      // })
+      // // sessions.sessionStartDate.getHours() + ":" + sessions.sessionStartDate.getMinutes() + ":" + sessions.sessionStartDate.getSeconds();
+      // // console.log(sessions)
+      // // console.log(sessions)
       res.render('private/overview', { sessionList: sessions })
     }).catch(err => {
       console.log(err);
@@ -61,4 +60,18 @@ router.get('/dashboard', middlewares.loginCheck, (req, res, next) => {
     })
   });
 
+  router.get('/close/:id', middlewares.loginCheck, (req, res, next) => {
+    // get all the sessions from the database
+    // console.log('test')
+    HelpSession.findByIdAndUpdate(req.params.id, {status: 'Closed', sessionEndDate: new Date()} )
+    .then(()=> {
+    HelpSession.find().populate('student')
+    .populate('teacher')
+    .then(sessions => {
+      res.render('private/overview', { sessionList: sessions })
+    }).catch(err => {
+      console.log(err);
+    })
+  })
+});
 module.exports = router;
